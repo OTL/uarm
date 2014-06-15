472d12eeaccb0e31a5ad0d7ea158780caf49e1fa
Takashi Ogura <t.ogura@gmail.com>
2014-06-15 22:27:11 +0900

    Update
    *remove library
    * update README
    * changed topic names into namespace /uarm/*

--- README.md
+++ README.md
@@ -1,9 +1,76 @@
-uarm_ros
-========
+uarm ros package
+=================
 
-ROS tools for UArm by UFactory
+ROS tools for UArm by UFactory.
 
-dependency
+
+Dependency
 --------------
 - rosserial
 - UF_uArm (arduino library)
+
+Install
+===============
+0. Install arduino sdk from Arduino.cc[http://arduino.cc/en/Main/Software].
+
+1. Install rosserial
+
+```bash
+    $ sudo apt-get install ros-hydro-rosserial
+```
+
+3. checkout uarm(this!) and build
+
+```bash
+    $ cd ~/catkin_ws/src
+    $ git clone https://github.com/OTL/uarm.git
+    $ cd ~/catkin_ws
+    $ catkin_make
+```
+
+4. generate ros_lib (ros message file for arduino)
+
+```bash
+    $ rosrun rosserial_arduino make_libraries.py ~/sketchbook/libraries
+```
+
+
+2. install UF_uArm arduino library
+
+```bash
+    $ cd  ~/sketchbook/libraries
+    $ git clone https://github.com/UFactory/UF_uArm.git
+```
+
+5. make a link to the sketch directory 
+
+```bash
+    $ ln -s ~/catkin_ws/src/uarm/uArmROS ~/sketchbook/
+```
+
+6. Start Arduino SDK, and install sketch, uArmROS
+
+  File -> Sketchbook -> uArmROS
+
+Run
+================
+
+```bash
+    $ rosrun rosserial_python serial_node.py
+    $ rostopic list
+```
+
+Pub/Sub
+=================
+
+Publish
+-----------
+* /uarm/button/d4 (std_msgs/Bool): button state (D4)
+* /uarm/button/d7 (std_msgs/Bool): button state (D7)
+* /uarm/joint_states (sensor_msgs/JointState): TODO: raw joint AD value
+
+Subscribe
+-----------
+* /uarm/joint_commands (uarm/Joints): move joints (unit looks [deg] and [mm])
+* /uarm/gripper (std_msgs/Bool): true -> hold, false -> release (only tested for pump)
+
--- uArmROS/uArmROS.ino
+++ uArmROS/uArmROS.ino
@@ -27,19 +27,33 @@ void gripperCallback(const std_msgs::Bool &command) {
 
 ros::NodeHandle nh;
 float positions[5] = {0.0};
-ros::Subscriber<std_msgs::Bool> gripper_sub("gripper", gripperCallback);
-ros::Subscriber<uarm::Joints> joint_sub("joint_commands", jointCallback);
+//char *names[5] = {"ROT", "L", "R", "HAND_ROT", "HAND"};
+
+ros::Subscriber<std_msgs::Bool> gripper_sub("/uarm/gripper", gripperCallback);
+ros::Subscriber<uarm::Joints> joint_sub("/uarm/joint_commands", jointCallback);
                                                    
 sensor_msgs::JointState joint_state_msg;
-ros::Publisher joint_state_pub("joint_states", &joint_state_msg);
+ros::Publisher joint_state_pub("/uarm/joint_states", &joint_state_msg);
+
+std_msgs::Bool button_d4_msg;
+ros::Publisher button_d4_pub("/uarm/button/d4", &button_d4_msg);
+
+std_msgs::Bool button_d7_msg;
+ros::Publisher button_d7_pub("/uarm/button/d7", &button_d7_msg);
 
 void setup() 
 {
-  uarm_robot.init();          // initialize the uArm position
+  uarm_robot.init();
+
   joint_state_msg.position_length = 5;
   joint_state_msg.position = positions;
+  //  joint_state_msg.name_length = 5;
+  //  joint_state_msg.name = names; // these causes freeze
+
   nh.initNode();
   nh.advertise(joint_state_pub);
+  nh.advertise(button_d4_pub);
+  nh.advertise(button_d7_pub);
   nh.subscribe(gripper_sub);
   nh.subscribe(joint_sub);
   delay(500);
@@ -52,8 +66,12 @@ void loop()
   joint_state_msg.position[2] = (float)uarm_robot.readAngle(SERVO_R);
   joint_state_msg.position[3] = (float)uarm_robot.readAngle(SERVO_HAND_ROT);
   joint_state_msg.position[4] = (float)uarm_robot.readAngle(SERVO_HAND);
+  button_d4_msg.data = (digitalRead(BTN_D4) == 0);
+  button_d7_msg.data = (digitalRead(BTN_D7) == 0);
+  button_d4_pub.publish(&button_d4_msg);
+  button_d7_pub.publish(&button_d7_msg);
   joint_state_pub.publish(&joint_state_msg);
   nh.spinOnce();
-  delay(100);
-  uarm_robot.gripperDetach();  
+  delay(50);
+  uarm_robot.gripperDetach();
 }
